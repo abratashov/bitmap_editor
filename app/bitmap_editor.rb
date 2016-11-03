2b6b58acdbb99acd2e40d1f2a233a18e7e9422f0
Oleksandr Bratashov <abratashov@gmail.com>
2016-11-03 18:50:44 +0200

    added command for drawing a vertical line

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -15,6 +15,8 @@ class BitmapEditor
           clean_image
         when /\s*L\s+\d+\s+\d+\s+[A-Z]\s*\z/
           draw_pixel(input)
+        when /\s*V\s+\d+\s+\d+\s+\d+\s+[A-Z]\s*\z/
+          draw_vertical_line(input)
         when /\s*S\s*\z/
           show
         when /\s*X\s*\z/
@@ -45,11 +47,28 @@ class BitmapEditor
     def draw_pixel(input)
       if @image
         params = input.split(' ')
-        @x, @y, @colour = params[1].to_i, params[2].to_i, params[3]
-        if @x.between?(1, @width) && @y.between?(1, @height)
-          @image[@y-1][@x-1] = @colour
+        x, y, colour = params[1].to_i, params[2].to_i, params[3]
+        if x.between?(1, @width) && y.between?(1, @height)
+          @image[y-1][x-1] = colour
         else
           puts "X should be between 1 and #{@width}, Y should be between 1 and #{@height} or enter '?' for help"
+        end
+      else
+        msg_empty_image
+      end
+    end
+
+    def draw_vertical_line(input)
+      if @image
+        params = input.split(' ')
+        x, y1, y2, colour = params[1].to_i, params[2].to_i, params[3].to_i, params[4]
+        if x.between?(1, @width) &&
+           y1.between?(1, @height) && y2.between?(1, @height) &&
+           y1 <= y2
+
+          (y1..y2).each{|y| @image[y-1][x-1] = colour}
+        else
+          puts "X should be between 1 and #{@width}, Y1/Y2 should be between 1 and #{@height} or enter '?' for help"
         end
       else
         msg_empty_image
