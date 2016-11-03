fade91bcd6ec0c7a0e2bfb2e6bfc816bdef15851
Oleksandr Bratashov <abratashov@gmail.com>
2016-11-03 20:33:08 +0200

    moved bitmap drawing logic into separate class

---
+++ app/bitmap.rb
@@ -0,0 +1,79 @@
+class Bitmap
+  def init_new_image(input)
+    params = input.split(' ')
+    @width, @height = params[1].to_i, params[2].to_i
+    @image = []
+    clean_image
+  end
+
+  def clean_image
+    if @image
+      @image = []
+      @height.times.each {|time| @image << 'O'*@width}
+    else
+      msg_empty_image
+    end
+  end
+
+  def draw_pixel(input)
+    if @image
+      params = input.split(' ')
+      x, y, colour = params[1].to_i, params[2].to_i, params[3]
+      if x.between?(1, @width) && y.between?(1, @height)
+        @image[y-1][x-1] = colour
+      else
+        message("X should be between 1 and #{@width}, Y should be between 1 and #{@height} or enter '?' for help")
+      end
+    else
+      msg_empty_image
+    end
+  end
+
+  def draw_vertical_line(input)
+    if @image
+      params = input.split(' ')
+      x, y1, y2, colour = params[1].to_i, params[2].to_i, params[3].to_i, params[4]
+      if x.between?(1, @width) &&
+         y1.between?(1, @height) && y2.between?(1, @height) &&
+         y1 <= y2
+
+        (y1..y2).each{|y| @image[y-1][x-1] = colour}
+      else
+        message("X should be between 1 and #{@width}, Y1/Y2 should be between 1 and #{@height} or enter '?' for help")
+      end
+    else
+      msg_empty_image
+    end
+  end
+
+  def draw_horizontal_line(input)
+    if @image
+      params = input.split(' ')
+      x1, x2, y, colour = params[1].to_i, params[2].to_i, params[3].to_i, params[4]
+      if x1.between?(1, @width) && x2.between?(1, @width) &&
+         x1 <= x2 &&
+         y.between?(1, @height)
+
+        (x1..x2).each{|x| @image[y-1][x-1] = colour}
+      else
+        message("X1/X2 should be between 1 and #{@width}, Y should be between 1 and #{@height} or enter '?' for help")
+      end
+    else
+      msg_empty_image
+    end
+  end
+
+  def show
+    @image.each{|row| puts row} if @image
+  end
+
+  private
+
+  def msg_empty_image
+    message("you must create image, enter '?' for help")
+  end
+
+  def message(msg)
+    { description: msg }
+  end
+end
--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,107 +1,40 @@
+load 'app/bitmap.rb'
+
 class BitmapEditor
 
   def run
     @running = true
+    bitmap = Bitmap.new
     puts 'type ? for help'
     while @running
       print '> '
       input = gets.chomp
+      @message = nil
       case input
         when '?'
           show_help
         when /\s*I\s+\d+\s+\d+\s*\z/
-          init_new_image(input)
+          @message = bitmap.init_new_image(input)
         when /\s*C\s*\z/
-          clean_image
+          @message = bitmap.clean_image
         when /\s*L\s+\d+\s+\d+\s+[A-Z]\s*\z/
-          draw_pixel(input)
+          @message = bitmap.draw_pixel(input)
         when /\s*V\s+\d+\s+\d+\s+\d+\s+[A-Z]\s*\z/
-          draw_vertical_line(input)
+          @message = bitmap.draw_vertical_line(input)
         when /\s*H\s+\d+\s+\d+\s+\d+\s+[A-Z]\s*\z/
-          draw_horizontal_line(input)
+          @message = bitmap.draw_horizontal_line(input)
         when /\s*S\s*\z/
-          show
+          @message = bitmap.show
         when /\s*X\s*\z/
           exit_console
         else
           puts "unrecognised command :( please enter '?' for help"
       end
+      puts @message[:description] if @message.class == Hash && @message[:description]
     end
   end
 
   private
-    def init_new_image(input)
-      params = input.split(' ')
-      @width, @height = params[1].to_i, params[2].to_i
-      @image = []
-      clean_image
-    end
-
-    def clean_image
-      if @image
-        @image = []
-        @height.times.each {|time| @image << 'O'*@width}
-      else
-        msg_empty_image
-      end
-    end
-
-    def draw_pixel(input)
-      if @image
-        params = input.split(' ')
-        x, y, colour = params[1].to_i, params[2].to_i, params[3]
-        if x.between?(1, @width) && y.between?(1, @height)
-          @image[y-1][x-1] = colour
-        else
-          puts "X should be between 1 and #{@width}, Y should be between 1 and #{@height} or enter '?' for help"
-        end
-      else
-        msg_empty_image
-      end
-    end
-
-    def draw_vertical_line(input)
-      if @image
-        params = input.split(' ')
-        x, y1, y2, colour = params[1].to_i, params[2].to_i, params[3].to_i, params[4]
-        if x.between?(1, @width) &&
-           y1.between?(1, @height) && y2.between?(1, @height) &&
-           y1 <= y2
-
-          (y1..y2).each{|y| @image[y-1][x-1] = colour}
-        else
-          puts "X should be between 1 and #{@width}, Y1/Y2 should be between 1 and #{@height} or enter '?' for help"
-        end
-      else
-        msg_empty_image
-      end
-    end
-
-    def draw_horizontal_line(input)
-      if @image
-        params = input.split(' ')
-        x1, x2, y, colour = params[1].to_i, params[2].to_i, params[3].to_i, params[4]
-        if x1.between?(1, @width) && x2.between?(1, @width) &&
-           x1 <= x2 &&
-           y.between?(1, @height)
-
-          (x1..x2).each{|x| @image[y-1][x-1] = colour}
-        else
-          puts "X1/X2 should be between 1 and #{@width}, Y should be between 1 and #{@height} or enter '?' for help"
-        end
-      else
-        msg_empty_image
-      end
-    end
-
-    def show
-      @image.each{|row| puts row} if @image
-    end
-
-    def msg_empty_image
-      puts "you must create image, enter '?' for help"
-    end
-
     def exit_console
       puts 'goodbye!'
       @running = false
